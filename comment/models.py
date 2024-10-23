8d65272b728a80c6d53182d80f79c9e9eb5b8a96
Yeryeong0101 <fud030101@gmail.com>
2024-10-23 13:39:53 +0900

    댓글 기능 구현

--- comment/models.py
+++ comment/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Comment(models.Model):
+    name = models.CharField(max_length=100)  # 작성자 이름
+    content = models.TextField()  # 댓글 내용
+    created_at = models.DateTimeField(auto_now_add=True)  # 댓글 작성 시간 자동 저장
+
+    def __str__(self):
+        return f'{self.name} - {self.created_at}'
+
--- comment/views.py
+++ comment/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import AllowAny
+from .models import Comment
+from .serializers import CommentSerializer
 
-# Create your views here.
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all().order_by('-created_at')  # 최신순으로 정렬
+    serializer_class = CommentSerializer
+    permission_classes = [AllowAny]  # 누구나 접근 가능
+    http_method_names = ['get', 'post']  # 조회(get)와 등록(post)만 허용
