21a3375d6e18c60ddb63e816a99114891fe12c65
Yeryeong0101 <fud030101@gmail.com>
2024-11-05 19:34:35 +0900

    글자수 제한

--- comment/models.py
+++ comment/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class Comment(models.Model):
-    name = models.CharField(max_length=100)  # 작성자 이름
-    content = models.TextField()  # 댓글 내용
+    name = models.CharField(max_length=10)  # 작성자 이름
+    content = models.TextField(max_length=150)  # 댓글 내용
     created_at = models.DateTimeField(auto_now_add=True)  # 댓글 작성 시간 자동 저장
 
     def __str__(self):
