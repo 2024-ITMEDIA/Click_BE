839037c547f99b3f75a0db285a26884cc1094bb1
mogg22 <ahrthdus1@gmail.com>
2024-11-09 15:07:36 +0900

    cors 추가

--- Click/settings.py
+++ Click/settings.py
@@ -45,9 +45,11 @@ INSTALLED_APPS = [
     'rest_framework',
     'comment',
     'media',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -55,6 +57,14 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+    'http://localhost:5173',
+    'http://127.0.0.1:8000',
+    'https://2024-dswu-itmedia.site/',
 ]
 
 ROOT_URLCONF = 'Click.urls'
